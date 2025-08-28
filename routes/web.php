2422074e96651fa85ee5aa44e13ea4c96f60d8ef
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2025-08-28 21:15:53 +0700

    add favicon + welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "author"   => "Delynessence",
+        "creator"   => "Shalman",
+        "contact"  => "www.linkedin.com/in/shalmanrafli",
+        "version"  => "0.0.1",
+    ], 200, ['Content-Type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 });
